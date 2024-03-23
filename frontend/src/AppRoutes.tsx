e918958fbee4836ce6656dbc2214bdbe273cd77e
Gr8fulnez <oluwolejohn72@gmail.com>
2024-03-23 12:38:39 -0700

    implement Auth0 Access Token in frontend

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layouts/layouts";
 import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={ <Layout><HomePage/></Layout>}/>
+            <Route path="/auth-callback" element={ <AuthCallbackPage/>}/>
             <Route path="/user-profile" element={ <span>User Profile age</span>}/>
             <Route path="/*" element={ <Navigate to="/"/>}/>
         </Routes>
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,13 +1,13 @@
 import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function Auth0ProviderWithNavigate({ children }: Props) {
-    const { createUser, } = useCreateMyUser();
-    
+const navigate = useNavigate()
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -15,11 +15,8 @@ export default function Auth0ProviderWithNavigate({ children }: Props) {
     throw new Error("unable to initialise auth");
   }
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-      // console.log("USER", user);
-      if (user?.sub && user?.email) {
-          createUser({ auth0Id: user.sub, email: user.email });
-          console.log(user,'here')
-      }
+    navigate("/auth-callback")
+    
   };
   return (
     <Auth0Provider
