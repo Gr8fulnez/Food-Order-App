4ac1f0d6cdf9faa3695d10ebccddca3643c81994
Gr8fulnez <oluwolejohn72@gmail.com>
2024-03-23 00:16:51 -0700

    implement mobile Nav

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./layouts/layouts";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={ <span>Home age</span>}/>
+            <Route path="/" element={ <Layout>HOME PAGE</Layout>}/>
             <Route path="/user-profile" element={ <span>User Profile age</span>}/>
             <Route path="/*" element={ <Navigate to="/"/>}/>
         </Routes>
